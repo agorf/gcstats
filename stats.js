078f186f7b9438070f475436698c5882e01b077d
Aggelos Orfanakos <agorf@agorf.gr>
2010-03-16 10:09:26 +0200

    Change indentation style in JavaScript file

--- stats.js
+++ stats.js
@@ -12,53 +12,59 @@ if (!GCStats) {
             var finds, finds_max;
 
             finds = $(table).find('td.finds');
-            finds_max = finds.
-                map(function () {
-                    return $(this).text() * 1;
-                }).get().max();
-            finds.
-                filter(function () {
-                    return $(this).text() * 1 === finds_max;
-                }).
-                each(function () {
-                    $(this).parent().find('td').addClass('hl b');
-                });
+
+            finds_max = finds.map(function () {
+                return $(this).text() * 1;
+            }).get().max();
+
+            finds.filter(function () {
+                return $(this).text() * 1 === finds_max;
+            }).each(function () {
+                $(this).parent().find('td').addClass('hl b');
+            });
         },
         highlight_square_table: function (table) {
             var total_row, row_max, total_col, col_max, inner_cells, table_max;
+
+            // total row
             
             total_row = $(table).find('tr.total td');
-            row_max = total_row.slice(0, -1).
-                map(function () {
-                    return $(this).text() * 1;
-                }).get().max();
-            total_row.addClass('hl').
-                filter(function () {
-                    return $(this).text() * 1 === row_max;
-                }).addClass('b');
+
+            row_max = total_row.slice(0, -1).map(function () {
+                return $(this).text() * 1;
+            }).get().max();
+
+            total_row.addClass('hl').filter(function () {
+                return $(this).text() * 1 === row_max;
+            }).addClass('b');
+
             total_row.last().addClass('b');
 
+            // total column
+
             total_col = $(table).find('td.total');
-            col_max = total_col.
-                map(function () {
-                    return $(this).text() * 1;
-                }).get().max();
-            total_col.addClass('hl').
-                filter(function () {
-                    return $(this).text() * 1 === col_max;
-                }).addClass('b');
+
+            col_max = total_col.map(function () {
+                return $(this).text() * 1;
+            }).get().max();
+
+            total_col.addClass('hl').filter(function () {
+                return $(this).text() * 1 === col_max;
+            }).addClass('b');
+
+            // inner cells
 
             inner_cells = $(table).find('td:not(.hl)');
-            table_max = inner_cells.
-                filter(function () {
-                    return ($(this).text() * 1).toString() === $(this).text();
-                }).map(function () {
-                    return $(this).text() * 1;
-                }).get().max();
-            inner_cells.
-                filter(function () {
-                    return $(this).text() * 1 === table_max;
-                }).addClass('hl b');
+
+            table_max = inner_cells.filter(function () {
+                return ($(this).text() * 1).toString() === $(this).text();
+            }).map(function () {
+                return $(this).text() * 1;
+            }).get().max();
+
+            inner_cells.filter(function () {
+                return $(this).text() * 1 === table_max;
+            }).addClass('hl b');
         },
     };
 
